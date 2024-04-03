9f4e81982436c0b975dd767f8e5f46b0bf5a1507
Vladimir Osipov <vladimir.osipov@coursio.com>
2024-04-04 00:21:09 +0300

    enhance an example to show user addition, listing and deletion

--- javascript/example.js
+++ javascript/example.js
@@ -1,9 +1,59 @@
 const { Lingolette } = require('./lingolette')
 
-const api = new Lingolette('YOUR_ORG_ID', 'YOUR_ORG_SECRET')
+const api = new Lingolette('YOUR_ORG_ID', 'YOUR_ORG_SECRET');
 
-api.call('org', 'createUserSession', {
-  userId: 'YOUR_ORG_USER_ID',
-})
-  .then(result => console.log(result))
-  .catch(error => console.log('API error', error))
+(async () => {
+  console.log('\n===\nCreate user session\n---')
+
+  try {
+    const result = await api.call('org', 'createUserSession', {
+      userId: 'YOUR_ORG_USER_ID',
+    })
+    console.log('Session token:', result.data?.token)
+  } catch (error) {
+    console.log('createUserSession API error:', error)
+  }
+
+  let newUserId = null
+
+  console.log('\n===\nAdd user\n---')
+  try {
+    const result = await api.call('org', 'addUser', {
+      name: 'John Doe',
+      targetLng: 'es',
+      nativeLng: 'en',
+      languageLevel: 0,
+    })
+
+    console.log('User added:', result)
+    newUserId = result.data?.id
+  } catch (error) {
+    console.log('addUser API error:', error)
+  }
+
+  console.log('\n===\nList users\n---')
+
+  try {
+    const result = await api.call('org', 'listUsers')
+
+    console.log('Org users:', result.data)
+  } catch (error) {
+    console.log('listUsers API error:', error)
+  }
+
+  if (!newUserId) {
+    return
+  }
+
+  console.log('\n===\nRemove user\n---')
+
+  try {
+    const result = await api.call('org', 'removeUser', {
+      userId: newUserId,
+    })
+
+    console.log('User removed:', result)
+  } catch (error) {
+    console.log('removeUser API error:', error)
+  }
+})()
